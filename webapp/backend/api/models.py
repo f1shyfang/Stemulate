1cd3d52fa9e2a7c518d6ffad2db05b71de4ba08b
mf <michaelf873@icloud.com>
2024-05-09 14:31:36 +1000

    tried adding csv to db doesnt seem to work

--- webapp/backend/api/models.py
+++ webapp/backend/api/models.py
@@ -15,21 +15,9 @@ class Note(models.Model):
         return self.title
 
 class Data(models.Model):
-    type = models.TextField()
-    element_id = models.TextField()
-    text = models.TextField()
-    coordinates_points = models.TextField()
-    coordinates_system = models.TextField()
-    coordinates_layout_width = models.TextField()
-    coordinates_layout_height = models.TextField()
-    file_directory = models.TextField()
-    filename = models.TextField()
-    languages = models.TextField()
-    last_modified = models.TextField()
-    page_number = models.TextField()
-    filetype = models.TextField()
-    parent_id = models.TextField()
-    links = models.TextField()
+    text = models.CharField(max_length=1000000)
+    filename = models.CharField(max_length=100000)
+    parent_id = models.CharField(max_length=10000)
     
     def __str__(self):
         return self.title
